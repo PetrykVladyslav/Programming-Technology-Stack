66c8b8b0e0c2d4e95c5658c29b7293248de88f61
PetrykVladyslav <160158113+PetrykVladyslav@users.noreply.github.com>
2024-09-12 18:38:27 +0200

    Add files via upload

---
+++ Rock-Paper-Scissors/rps.rb
@@ -0,0 +1,25 @@
+choice = ["Rock", "Paper", "Scissors"]
+
+puts "Оберіть свій варіант: Rock, Paper, Scissors"
+user_pick = gets.chomp.capitalize.strip
+
+while !choice.include?(user_pick) do
+  puts "Невірний вибір. Спробуйте ще раз."
+  puts "Оберіть свій варіант: Rock, Paper або Scissors"
+  user_pick = gets.chomp.capitalize.strip
+end
+
+puts "Ви обрали: #{user_pick}"
+
+computer_pick = choice.sample
+puts "Комп'ютер обрав: #{computer_pick}"
+
+if user_pick == computer_pick
+  puts "Нічия!"
+elsif (user_pick == "Rock" && computer_pick == "Scissors") ||
+  (user_pick == "Scissors" && computer_pick == "Paper") ||
+  (user_pick == "Paper" && computer_pick == "Rock")
+  puts "Ви перемогли!"
+else
+  puts "Комп'ютер переміг!"
+end
